9d98074d42f6d39021a7ae76d43a9115d5b5e095
Robin S <52407878+robinsardella@users.noreply.github.com>
2020-02-17 13:05:56 +0100

    First commit

---
+++ src/test/java/com/example/Financeproject/FinanceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Financeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
